75d4caa204cb5975b02ce504912f8e64e47b11a8
fernandobouchet <fernandobouchet@gmail.com>
2024-03-09 17:52:55 -0300

    feat: add 'next-themes' dependencies and configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
+import { ThemeProvider } from '@/providers/ThemeProvider';
 import './globals.css';
 import { cn } from '@/lib/utils';
 
@@ -19,14 +20,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
