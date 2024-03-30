0ae8121d2912c0df735ae928af6622ca67ac8e8c
fernandobouchet <fernandobouchet@gmail.com>
2024-03-30 17:22:11 -0300

    feat: add 'linkButton' component

---
+++ components/ui/linkButton.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  href: string | null;
+  children: string | null | undefined;
+}
+
+const LinkButton = ({ children, href }: Props) => {
+  return href !== null && href !== undefined ? (
+    <Link
+      href={href!}
+      className={cn(buttonVariants({ variant: "default" }))}
+      target="_blank"
+    >
+      {children}
+    </Link>
+  ) : (
+    <Button>{children}</Button>
+  );
+};
+
+export { LinkButton };
