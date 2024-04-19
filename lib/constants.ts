7d8981742abc3affd4a3648656a03f0fef9df158
fernandobouchet <fernandobouchet@gmail.com>
2024-04-19 00:26:53 -0300

    feat: add 'constants' utils

---
+++ lib/constants.ts
@@ -0,0 +1,28 @@
+const careers = [
+  {
+    id: 1,
+    title: "Licenciatura en informática",
+  },
+  {
+    id: 2,
+    title: "Tecnicatura en informática",
+  },
+  {
+    id: 3,
+    title: "Tecnicatura en programación",
+  },
+  {
+    id: 4,
+    title: "Tecnicatura en redes y operaciones",
+  },
+  {
+    id: 5,
+    title: "Tecnicatura en inteligencia artificial",
+  },
+  {
+    id: 6,
+    title: "Tecnicatura en videojuegos",
+  },
+];
+
+export { careers };
