9aedd29b276aa94cb5138449c6645e298fddafad
fernandobouchet <fernandobouchet@gmail.com>
2024-06-18 23:18:20 -0300

    feat: create migration with latest schema changes

---
+++ prisma/migrations/20240619021809_remove_approval_year/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `approvalYear` on the `UserCourse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserCourse" DROP COLUMN "approvalYear";
